460ed9e5530f56967e63ba22581865e5aa3c5794
dins <dinssimon@gmail.com>
2020-05-10 21:54:32 +0530

    10/5/2020

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { UserBrief } from './brief.component';
+import {UserDetails } from './details.component'
 
 @NgModule({
     imports:[BrowserModule],
-    declarations:[AppComponent],
+    declarations:[AppComponent,UserBrief,UserDetails],
     bootstrap:[AppComponent]
 })
 export class AppModule{}
---
+++ src/app/brief.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+        selector:`user-details`,
+        templateUrl:`./brief.component.html`,
+        styleUrls:[`./brief.component.css`]
+    }
+)
+
+export class UserBrief{}
